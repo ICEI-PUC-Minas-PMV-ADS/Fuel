c7cf13709cafe722c76818d47849ff21c36f7696
RickyPaulo <126209236+rickypaulo@users.noreply.github.com>
2024-05-01 09:19:44 +1000

    Update DbServices.js

--- services/DbServices.js
+++ services/DbServices.js
@@ -9,7 +9,7 @@ const Database = {
         
         db.transaction((tx) => {
 
-            tx.executeSql('create table if not exists estabelecimentos(id intreger primary key not null, nome string not null, cnpj string not null, endereco string not null, telefone string not null, tipoCombustivel string not null, preco real not null, bandeiraPosto string not null, outrosServicos string not null)');
+            tx.executeSql('create table if not exists estabelecimentos(id integer primary key not null, nome text not null, cnpj string not null, endereco string not null, telefone string not null, tipoCombustivel string not null, preco real not null, bandeiraPosto string not null, outrosServicos string not null)');
         });
 
         const ExecuteQuery = (sql, params = []) =>
